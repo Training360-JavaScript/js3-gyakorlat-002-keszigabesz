a836e1c480b038f9c4c9778bf6b7b12f9f54a414
keszigabesz <bajo.gabor@gmail.com>
2021-11-11 13:10:22 +0100

    kész

--- solutions/app2.js
+++ solutions/app2.js
@@ -36,6 +36,22 @@ const cookieHandler = {
         }
         return cookieObject;
     },
+    toSessionStorage() {
+        const cookieNameValuePairs = document.cookie.split('; ');
+        console.log(cookieNameValuePairs);
+        cookieNameValuePairs.forEach(element => {
+            const nameAndValueArray = element.split('=');
+            sessionStorage.setItem(nameAndValueArray[0], nameAndValueArray[1]);
+        });
+    },
+    flush() {
+        const cookieNameValuePairs = document.cookie.split('; ');
+        cookieNameValuePairs.forEach(element => {
+            const nameAndValueArray = element.split('=');
+            document.cookie =
+                `${nameAndValueArray[0]}=;expires = Thu, 01 Jan 1970 00:00:00 UTC;path=/`;
+        });
+    }
 }
 
 export {
