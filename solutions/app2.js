72d87abda501793bd7782ae2f75cda05d225c09a
keszigabesz <bajo.gabor@gmail.com>
2021-11-11 13:11:45 +0100

    kész

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,20 +1,3 @@
-/* Fájl: `solutions/app2.js`
-- Objektum neve: `cookieHandler`
-- Export: `export { cookieHandler };`
-- Az alábbi cookie-k legyenek a böngésződben tárolva 
-(az első feladatban szereplő setCookie függvénnyel hozd őket létre, egyszerűen
-másold át a függvényt ebbe a feladatba is, és futtasd háromszor):
-  - viewed: 5
-  - uid: 354774631237
-  - ssid: Bx55OWbHJ0Vt_IGIF
-  
-- A cookieHandler objektum az alábbi három metódust tartalmazza:
-  - `getAll`: kiolvassa a sütik nevét és értékét, majd visszaadja őket egy 
-objektumban, ahol a sütik neve a key és az értéke a value (pl. {viewed: 5}).
-  - `toSessionStorage`: minden sütit egyenként elment a sessionStorage-ba az 
-adott süti nevével és értékével.
-  - `flush`: törli az összes sütit. */
-
 const setCookie = (cookieName, cookieValue) => {
     const now = new Date();
     now.setTime(now.getTime() + (15 * 60 * 1000));
